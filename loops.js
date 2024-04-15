fe466db7dc507584a0ed84cb660fe72bb2f1412d
Disha Ray <94073736+Disha-Ray@users.noreply.github.com>
2024-04-15 23:46:55 +0530

    Update loops.js

--- loops.js
+++ loops.js
@@ -6,3 +6,23 @@ for(let i = 1; i <= 4; i++) {
 // for ([initialization]; [condition]; [update]) {
    // statement
 //}
+
+// find a factorial of a given number n
+function factorial(n){
+    let res=1;
+    for(let i=1;i<=n;i++){
+        res*=i;
+    }
+    return res;
+}
+const ans=factorial(5);
+console.log(ans);
+
+// loops in string
+//  Let's add some exclamation marks to "Hello World"!
+
+let str = "Hello World";
+for(let i = 1; i <= 5; i++) {
+    str += "!";
+}
+console.log(str); // Hello World!!!!!
