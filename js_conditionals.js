052d0e900bb51dd14602bd38c68efa9d4eddc3c3
Disha Ray <94073736+Disha-Ray@users.noreply.github.com>
2024-04-15 14:03:19 +0530

    Update js_conditionals.js

--- js_conditionals.js
+++ js_conditionals.js
@@ -14,3 +14,33 @@ console.log(isEqual(2,2));
 function isEqual(a,b){
   return a===b;
 }
+// else if
+if(a > b) {
+    console.log('a is greater!');
+}
+else if (b > a) {
+    console.log('b is greater!');
+} 
+else {
+    console.log('they are the same!');
+}
+
+// we can write the above else if because it is short hand of the below one
+if(a > b) {
+    console.log('a is greater!');
+}
+else {
+    if(b > a) {
+        console.log('b is greater!');
+    }
+    else {
+        console.log('they are the same!');
+    }
+}
+
+// The reason this works is because we don't actually need the braces for an if or else statement:
+
+if(a > b) 
+    console.log('a is greater');
+
+//Typically, braces {} are encouraged for if/else statements.
